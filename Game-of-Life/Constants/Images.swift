bc35ea17d65d11c792b06f4a73b047411e7fa398
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-19 16:14:06 +0800

    - added Images - updated color of TitleView

---
+++ Game-of-Life/Constants/Images.swift
@@ -0,0 +1,27 @@
+//
+//  Images.swift
+//  Game-of-Life
+//
+//  Created by Ronjie Diafante Man-on on 5/19/25.
+//
+
+import SwiftUI
+
+struct BackgroundImages {
+    static let all: [ImageResource] = [
+        .autumn1,
+        .autumn2,
+        .autumn3,
+        .mountain1,
+        .mountain2,
+        .mountain3,
+        .space1,
+        .space2,
+        .underwater1,
+        .underwater2,
+        .underwater3,
+        .cyber1,
+        .cyber2,
+        .code1
+    ]
+}
--- Game-of-Life/View/Sub View/TitleView.swift
+++ Game-of-Life/View/Sub View/TitleView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct TitleView: View {
+    var gradientColors: [Color] {
+        [.gradColor1, .gradColor2, .gradColor3]
+    }
+    
     var body: some View {
         Text("Game of Life")
             .font(.largeTitle)
-            .fontWeight(.bold)
+            .bold()
+            .foregroundStyle(
+                LinearGradient(
+                    colors: gradientColors,
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+            .shadow(
+                color: .shadow,
+                radius: 10,
+                x: 0,
+                y: 5
+            )
     }
 }
 
