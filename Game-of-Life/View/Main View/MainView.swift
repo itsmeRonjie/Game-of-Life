481be3e541d083f8849bbe24b87c985be0a584d6
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-19 16:42:27 +0800

    - added start game and timer

--- Game-of-Life/View/Main View/MainView.swift
+++ Game-of-Life/View/Main View/MainView.swift
@@ -13,6 +13,25 @@ struct MainView: View {
     @State private var swapXY: Bool = false
     @State private var currentImage: ImageResource = .mountain1
     @State private var showImage: Bool = true
+    @State private var isPlaying: Bool = true
+
+    @State private var timer: Timer?
+    
+    let MIN_SECONDS: Double = 0.01
+    let MAX_SECONDS: Double = 3.0
+    let MAX_SPEED: Double = 10.0
+    var MAX_TIME: Double {
+        MAX_SPEED / 2
+    }
+    
+    var a: Double {
+        (MIN_SECONDS - MAX_SECONDS) / MAX_SPEED
+    }
+    
+    var speed: Double {
+        1 / a * (MAX_TIME - MAX_SECONDS)
+    }
+
     
     var body: some View {
         ZStack {
@@ -36,6 +55,25 @@ struct MainView: View {
             }
             .padding()
         }
+        .onAppear { startGame() }
+        .onChange(of: speed) { _, newSpeed in
+            // TODO: Reset Speed
+        }
+    }
+    
+    func startGame() {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 1 / speed,
+            repeats: true,
+            block: { _ in
+                withAnimation {
+                    if isPlaying {
+                        boardModel.nextGeneration()
+                    }
+                }
+            }
+        )
     }
 }
 
